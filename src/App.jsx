639e6f3dde14423e0356bcf405ca1f909ce84c42
Hemanth Taduka <38909957+blue-gitty@users.noreply.github.com>
2023-10-16 10:21:54 -0400

    added multi page functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import './App.css'
 import Greet from './components/main_page'
 import NavBar from './components/nav_bar'
 import HeroSection from './components/Hero_container'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
 
 
 function App() {
@@ -12,31 +15,19 @@ function App() {
   console.log(JSON.stringify(import.meta.env.VITE_REACT_APP_ENV), "dskjdksjdkskjdsk");
 
   return (
-    <div className='container'>
+    <>
     <NavBar/>
-    <HeroSection/>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div> */}
+    <div className='container'>
       
-      
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+{/*      
+      <Routes>
+
+        <Route path='/' element={<Home/>} />
+
+      </Routes> */}
+    
     </div>
+    </>
   )
 }
 
--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import cara_logo from "../assets/CARA_sys_logo.png"
 const NavBar = () => {
+
+
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <img src= {cara_logo}/>
+        <img src= {cara_logo} alt= 'cara_logo'/>
       </div>
-      {/* <ul className="navbar-links">
-        <li className="navbar-link"><a href="#">Solutions</a></li>
-        <li className="navbar-link"><a href="#">About</a></li>
-        <li className="navbar-link"><a href="#">Contact Us</a></li>
-      </ul> */}
+      <ul className="navbar-links">
+        {/* <li className="navbar-link" on><a href="/solutions">Solutions</a></li> */}
+        <li className="navbar-link"><a href="/about">About</a></li>
+        <li className="navbar-link"><a href="/contactus">Contact Us</a></li>
+      </ul>
     </nav>
   );
 };
