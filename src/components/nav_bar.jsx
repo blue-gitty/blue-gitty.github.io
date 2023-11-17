951813b3808c29e3326b99853d4713fd68011917
Hemanth Taduka <38909957+blue-gitty@users.noreply.github.com>
2023-11-17 15:56:20 -0500

    fixed cara logo

--- src/components/nav_bar.jsx
+++ src/components/nav_bar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import cara_logo from "../assets/CARA_sys_logo.png"
+import cara_logo from "../assets/logo_asset.svg"
 const NavBar = () => {
 
 
@@ -7,13 +7,14 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <img src= {cara_logo} alt= 'cara_logo'/>
+        <img  src= {cara_logo} alt= 'cara_logo' style={{ width: '120px', height: 'auto' }}  />
       </div>
       <ul className="navbar-links">
         {/* <li className="navbar-link"><a href="#">Solutions</a></li> */}
         {/* <li className="navbar-link"><a href="#">About</a></li> */}
         {/* <li className="navbar-link"><a href="#">Contact Us</a></li> */}
       </ul>
+
     </nav>
   );
 };
